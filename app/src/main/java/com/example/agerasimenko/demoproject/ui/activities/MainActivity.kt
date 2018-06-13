0ed7ddb71f929a7cd41d23b70b459770307c68e2
GerasimenkoAnton <a.gerasimenko@itransition.com>
2018-06-13 14:53:41 +0300

    Move the MainActivity to the .ui.activities package

--- app/src/main/java/com/example/agerasimenko/demoproject/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/agerasimenko/demoproject/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.agerasimenko.demoproject
+package com.example.agerasimenko.demoproject.ui.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.agerasimenko.demoproject.R
 
 class MainActivity : AppCompatActivity() {
 
