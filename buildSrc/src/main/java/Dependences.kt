1a740ade03ca9525e7121b425b156bdbcd8cefc4
GerasimenkoAnton <a.gerasimenko@itransition.com>
2018-06-14 18:30:13 +0300

    Connect the FireStore
    Do not forget add 'android.permission.INTERNET'

--- buildSrc/src/main/java/Dependences.kt
+++ buildSrc/src/main/java/Dependences.kt
@@ -4,21 +4,22 @@ package com.example.agerasimenko.demoproject
 object PluginDependences {
     const val android = "com.android.tools.build:gradle:${Versions.gradleAndroidPlugin}"
     const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+    const val googleService = "com.google.gms:google-services:${Versions.googleService}"
 }
-
 
 object ProjectDependences {
     const val applicationId = "com.example.agerasimenko.demoproject"
     const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
     const val appCompatV7 = "com.android.support:appcompat-v7:${Versions.appCompatV7}"
-    const val constrainLayout = "com.android.support.constraint:constraint-layout:${Versions.constrain}"
     const val junit = "junit:junit:${Versions.junit}"
     const val daggerCompiler = "com.google.dagger:dagger-compiler:${Versions.dagger}"
     const val dagger = "com.google.dagger:dagger:${Versions.dagger}"
+    const val firestore = "com.google.firebase:firebase-firestore:${Versions.firestore}"
 }
 
 
 object Versions {
+    const val googleService = "3.2.0"
     const val gradleAndroidPlugin = "3.1.3"
     const val kotlin = "1.2.41"
     const val minSdk = 15
@@ -27,7 +28,7 @@ object Versions {
     const val name = "1.0"
     const val compileSdk = 27
     const val appCompatV7 = "27.1.1"
-    const val constrain = "1.1.1"
     const val junit = "4.12"
     const val dagger = "2.15"
+    const val firestore = "17.0.1"
 }
