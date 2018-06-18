ffc5c039724931f65d66a56fb2fe209dbce50605
GerasimenkoAnton <a.gerasimenko@itransition.com>
2018-06-18 17:15:06 +0300

    Create the simple sender for testing Firestore
    Each 10 sec, during 4 hours it will create new document and read existing document

--- app/src/main/java/com/example/agerasimenko/demoproject/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/agerasimenko/demoproject/ui/activities/MainActivity.kt
@@ -8,6 +8,8 @@ import com.example.agerasimenko.demoproject.R
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
+import android.os.CountDownTimer
+import java.util.concurrent.TimeUnit
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,21 +17,42 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var db: FirebaseFirestore
 
+    val time = TimeUnit.HOURS.toMillis(4)
+
+    private val timer = object : CountDownTimer(time, 1000) {
+
+        var count = 0
+
+        override fun onTick(millisUntilFinished: Long) {
+            count++
+            readCollection("service", count)
+            writeCollection("review", count)
+        }
+
+        override fun onFinish() {
+            Log.d("FireStore", "fin")
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         (application  as MainApp).getApplicationComponent().inject(this)
         setContentView(R.layout.activity_main)
 
-
         get_data_fb.setOnClickListener {
-            readCollection("service")
-            readCollection("review")
+            get_data_fb.isEnabled = false
+            timer.start()
+        }
+        stop.setOnClickListener {
+            timer.cancel()
+            Log.d("FireStore", "cancel")
         }
     }
 
-    private fun readCollection(collectionName: String) {
+    private fun readCollection(collectionName: String, count: Int) {
         db.collection(collectionName).get().addOnCompleteListener {
             it.run {
+                Log.d("FireStore", "attempt $count")
                 if (isSuccessful) {
                     result.run {
                         Log.d("FireStore", "print $collectionName")
@@ -42,5 +65,16 @@ class MainActivity : AppCompatActivity() {
                 } else Log.e("FireStore", exception.toString())
             }
         }
+    }
+
+    private fun writeCollection(collectionName: String, count: Int)  {
+        val data = HashMap<String, Any>()
+        data["reviewer"] = count
+
+        db.collection(collectionName).document("W_$count")
+                .set(data)
+                .addOnSuccessListener {
+                    Log.d("FireStore", "successful $count")
+                }
     }
 }
