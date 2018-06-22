86c6e94192e8ca0b905cff0ea9f3b6c389b206dc
GerasimenkoAnton <a.gerasimenko@itransition.com>
2018-06-22 14:03:14 +0300

    Use the binder service in the MainActivity

--- app/src/main/java/com/example/agerasimenko/demoproject/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/agerasimenko/demoproject/ui/activities/MainActivity.kt
@@ -1,13 +1,34 @@
 package com.example.agerasimenko.demoproject.ui.activities
 
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
+import android.content.ServiceConnection
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import com.example.agerasimenko.demoproject.R
+import com.example.agerasimenko.demoproject.domain.BindService
 import com.example.agerasimenko.demoproject.domain.SimpleService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val serviceConnection = object : ServiceConnection {
+        override fun onServiceDisconnected(name: ComponentName?) {
+            "onServiceDisconnected".logd()
+        }
+
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            "onServiceConnected".logd()
+
+            val service = (service as? BindService.LocalBinder)?.getService() ?: return
+
+            service.getMessage().logd()
+        }
+    }
+
+    private var isRun = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,11 +39,23 @@ class MainActivity : AppCompatActivity() {
         }
         button_stop.setOnClickListener {
             stop()
+            if (isRun) {
+                stopUnbinService()
+            }
+        }
+
+        button_run_bind_service.setOnClickListener {
+            isRun = bindService(Intent(this, BindService::class.java), serviceConnection, Context.BIND_AUTO_CREATE)
         }
     }
 
     private fun stop() {
         stopService(Intent(this, SimpleService::class.java))
+    }
+
+    private fun stopUnbinService() {
+        unbindService(serviceConnection)
+        isRun = false
     }
 
     private fun runService() {
