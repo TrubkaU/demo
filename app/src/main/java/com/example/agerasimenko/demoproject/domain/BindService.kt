603ab83676589dc7e92c4552b684e23220f1e377
GerasimenkoAnton <a.gerasimenko@itransition.com>
2018-06-22 14:01:35 +0300

    Return service instance after binding

--- app/src/main/java/com/example/agerasimenko/demoproject/domain/BindService.kt
+++ app/src/main/java/com/example/agerasimenko/demoproject/domain/BindService.kt
@@ -3,14 +3,15 @@ package com.example.agerasimenko.demoproject.domain
 import android.app.Service
 import android.content.Intent
 import android.os.Binder
+import android.os.IBinder
 import com.example.agerasimenko.demoproject.ui.activities.logd
 
 
 class BindService: Service() {
 
-    private val iBinder= Binder()
+    private val iBinder = LocalBinder()
 
-    override fun onBind(intent: Intent?) = iBinder
+    override fun onBind(intent: Intent?): IBinder = iBinder
 
     override fun onCreate() {
         super.onCreate()
@@ -26,5 +27,13 @@ class BindService: Service() {
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         "onStartCommand  BindService".logd()
         return super.onStartCommand(intent, flags, startId)
+    }
+
+    fun getMessage() = "Service ready to use"
+
+    internal inner class LocalBinder: Binder() {
+        fun getService() : BindService {
+            return this@BindService
+        }
     }
 }
