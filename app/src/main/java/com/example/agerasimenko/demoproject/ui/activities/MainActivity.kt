ba36645bc0031f05c61ecbe01351fd0fb7cc62d5
GerasimenkoAnton <a.gerasimenko@itransition.com>
2018-06-29 15:31:24 +0300

    Add the "SHOW" and "CLOSE" button on the MainActivity

--- app/src/main/java/com/example/agerasimenko/demoproject/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/agerasimenko/demoproject/ui/activities/MainActivity.kt
@@ -3,11 +3,21 @@ package com.example.agerasimenko.demoproject.ui.activities
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.agerasimenko.demoproject.R
+import com.example.agerasimenko.demoproject.ui.view.CustomView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button_close.setOnClickListener {
+            custom_view_container.removeAllViews()
+        }
+
+        button_show.setOnClickListener {
+            custom_view_container.addView(CustomView(this))
+        }
     }
 }
